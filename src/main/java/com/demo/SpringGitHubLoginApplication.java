b82b59abd8bc0a6f1ac5e7b0797a8253fcf61aaf
Omkar Shivadekar <omkarshivadekar.os@gmail.com>
2022-01-15 01:56:18 +0100

    #3 - Astra DB connection infrastructure

--- src/main/java/com/demo/SpringGitHubLoginApplication.java
+++ src/main/java/com/demo/SpringGitHubLoginApplication.java
@@ -1,7 +1,11 @@
 package com.demo;
 
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +19,18 @@ public class SpringGitHubLoginApplication {
 		SpringApplication.run(SpringGitHubLoginApplication.class, args);
 	}
 
-	@RequestMapping("/user")
-	public String user(@AuthenticationPrincipal OAuth2User principal) {
-		System.out.println(principal);
-		return principal.getAttribute("name");
+	 @RequestMapping("/user")
+	 public String user(@AuthenticationPrincipal OAuth2User principal) {
+	 	System.out.println(principal);
+	 	return principal.getAttribute("name");
+	 }
+
+	/* This is necessary to have the Spring boot app use the astra secure bundle 
+	to connect to the database */
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties){
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
 	}
 	
 
